085e23a2e66c05152bd0aea48bc973700142ced5
Lordwick5 <Prashantchy96@gmail.com>
2026-07-14 15:00:15 +0545

    Allow Vercel frontend origin in CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,15 @@ import expenseRoutes from "./routes/expenseRoutes.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://expense-tracker-ndi4v6kxj-prashant-chaudharys-projects-133fa17b.vercel.app/",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json()); //parses incoming request bodies formatted as JSON
 
 app.use("/api/auth", authRoutes);
